af51eb94b79a1cf4ee91b12b0f86e84a92e1ba11
Tamir <tamir@htld>
2022-09-16 14:27:25 +0200

    Arrays

--- src/Arrays/Aufgabe2.java
+++ src/Arrays/Aufgabe2.java
@@ -5,10 +5,14 @@ import java.util.Random;
 public class Aufgabe2 {
     public static void main(String[] args) {
         Random random = new Random();
-        int rnumber = random.nextInt(0,100);
-        System.out.println(rnumber);
 
-        int [] array = {};
-
+        int [] array = new int[50];
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            array [i] = random.nextInt(0,100);
+            System.out.println(array[i]);
+            sum += array[i];
+        }
+        System.out.println("total of all random numbers: " + sum);
     }
 }
