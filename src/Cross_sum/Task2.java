5420f978dfde62e943fb9e1ce17f65ec28fdb31b
Tamir <tamir@htld>
2022-10-11 11:22:31 +0200

    Crosssum Task2

---
+++ src/Cross_sum/Task2.java
@@ -0,0 +1,22 @@
+package Cross_sum;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 1000; i++) {
+            String numberAsString = Integer.toString(i);
+            char[] numbers = numberAsString.toCharArray();
+
+            int sum = 0;
+            for (int cs = 0; cs < numbers.length; cs++) {
+                sum = sum + Character.getNumericValue(numbers[cs]);
+
+                if (sum % 7 == 0){
+                    System.out.println("number: " + i + "    crossum: " + sum);
+                }
+            }
+            }
+        }
+    }
+
+
