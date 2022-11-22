662ce8da0c734e20ba6ecb6683ef2a535872661d
Tamir <tamir@htld>
2022-11-22 11:23:50 +0100

    TikTakToe

---
+++ src/at/tamir/java/basics/TicTacToe/TikTakToe.java
@@ -0,0 +1,11 @@
+package at.tamir.java.basics.TicTacToe;
+
+import java.util.Scanner;
+
+public class TikTakToe {
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean finish = false;
+
+    }
+}
