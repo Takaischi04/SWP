ced7e8d9f37c9f6c71432ce941ae3c499c547048
Tamir <tamir@htld>
2022-12-06 10:06:05 +0100

    Car

---
+++ src/at/tamir/java/oo/Car/Car.java
@@ -0,0 +1,8 @@
+package at.tamir.java.oo.Car;
+
+public class Car {
+    public int fuelConsumption;
+    public String brand;
+    public String serialNumber;
+    private String color;
+}
---
+++ src/at/tamir/java/oo/Car/Car_Main.java
@@ -0,0 +1,9 @@
+package at.tamir.java.oo.Car;
+public class Car_Main{
+
+    public static void main(String[]args ){
+        int a = 7;
+    }
+
+}
+
